fafe3ec0015242097a3c47d07ee2d63bf8749dbe
urkohajko <urkohajko@gmail.com>
2026-05-16 17:04:48 +0200

    Switch to Twitter API v2

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ API_SECRET = os.getenv("X_API_SECRET")
 ACCESS_TOKEN = os.getenv("X_ACCESS_TOKEN")
 ACCESS_SECRET = os.getenv("X_ACCESS_SECRET")
 
-auth = tweepy.OAuth1UserHandler(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
-api = tweepy.API(auth)
+client = tweepy.Client(
+    consumer_key=API_KEY,
+    consumer_secret=API_SECRET,
+    access_token=ACCESS_TOKEN,
+    access_token_secret=ACCESS_SECRET
+)
 
 def post_deal(text):
-    api.update_status(text)
+    client.create_tweet(text=text)
 
 print("Bot de SaaS Deals iniciado...")
 
