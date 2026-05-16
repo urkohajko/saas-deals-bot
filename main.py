e5f3a54ab71bcd44e3be35fa85147c03fd4058f9
urkohajko <urkohajko@gmail.com>
2026-05-16 18:57:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 import tweepy
 import os
 
-api_key = os.getenv("X_API_KEY")
-api_secret = os.getenv("X_API_SECRET")
-access_token = os.getenv("X_ACCESS_TOKEN")
-access_secret = os.getenv("X_ACCESS_SECRET")
-
-auth = tweepy.OAuth1UserHandler(
-    api_key,
-    api_secret,
-    access_token,
-    access_secret
+client = tweepy.Client(
+    consumer_key=os.getenv("X_API_KEY"),
+    consumer_secret=os.getenv("X_API_SECRET"),
+    access_token=os.getenv("X_ACCESS_TOKEN"),
+    access_token_secret=os.getenv("X_ACCESS_SECRET")
 )
 
-api = tweepy.API(auth)
-
-api.update_status("🔥 Oferta SaaS del día: mensaje de prueba automático")
+client.create_tweet(text="🔥 Oferta SaaS del día: mensaje de prueba automático")
