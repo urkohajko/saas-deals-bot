58e44266f0c5effcc019ba56dc261e38ee0cfbdf
urkohajko <urkohajko@gmail.com>
2026-05-18 17:54:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from atproto import Client
 import os
+from modules.publisher import build_post
 
 def run_bot():
     username = os.getenv("BSKY_USERNAME")
@@ -8,11 +9,8 @@ def run_bot():
     client = Client()
     client.login(username, password)
 
-    # Cargar contenido desde tu módulo real
-    from modules.publisher import build_post
-
-    post_text = build_post()
-    client.send_post(post_text)
+    post = build_post()
+    client.send_post(post)
 
 if __name__ == "__main__":
     run_bot()
