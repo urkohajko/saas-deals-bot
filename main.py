9e9a0a8dcc374680ecee53651427cfe3bf623166
urkohajko <urkohajko@gmail.com>
2026-05-18 17:25:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import os
 username = os.getenv("BSKY_USERNAME")
 password = os.getenv("BSKY_APP_PASSWORD")
 
-print("USERNAME:", username)
-print("PASSWORD:", password)
+print("USERNAME:", repr(username))
+print("PASSWORD:", repr(password))
 
 client = Client()
 client.login(username, password)
