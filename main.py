f1949b4d041f5117375b4742bf87648619c6c3ac
urkohajko <urkohajko@gmail.com>
2026-05-18 18:29:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -51,25 +51,3 @@ def run_bot():
         parent = root_post
         for part in thread[1:]:
             embed = None
-            if part.get("image_url"):
-                img_bytes = download_image(part["image_url"])
-                if img_bytes:
-                    blob = client.upload_blob(img_bytes)
-                    embed = client.models.AppBskyEmbedImages.Main(
-                        images=[
-                            client.models.AppBskyEmbedImages.Image(
-                                image=blob.blob,
-                                alt=part.get("alt", part["text"][:80]),
-                            )
-                        ]
-                    )
-
-            parent = client.send_post(
-                part["text"],
-                embed=embed,
-                reply_to=parent.uri,
-            )
-
-
-if __name__ == "__main__":
-    run_bot()
